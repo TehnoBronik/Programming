1d605edae37e0e4b9d7da624f6ed4c814f289620
misha <mishamur21@gmail.com>
2018-12-02 20:59:43 +0300

    you

---
+++ rku.py
@@ -0,0 +1,13 @@
+from math import*
+print ("vvedite koefficents")
+a, b, c = list(map(float,input().split()))
+diskr=b*b-4*a*c
+if diskr<0:
+    print("korney net")
+elif diskr==0:
+    x=-b/(2*a)
+    print("odin koren",x)
+else: #
+    x1=(-b+sqrt(diskr))/(2*a)
+    x2=(-b-sqrt(diskr))/(2*a)
+    print("perviy koren = ", x1," vtoroy koren = ", x2)
