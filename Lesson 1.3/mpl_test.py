317d3add54c00ce24dd3aa83e50af676c7aec6a3
TehnoBronik <mishamur21@gmail.com>
2019-02-24 20:57:06 +0300

    y

--- Lesson 1.3/mpl_test.py
+++ Lesson 1.3/mpl_test.py
@@ -1,14 +1,2 @@
-%matplotlib inline
-
-import os
-import matplotlib.pyplot as plt
-import numpy as np
-
-def save(name='', fmt='png'):
-    pwd = os.getcwd()
-    iPath = './pictures/{}'.format(fmt)
-    if not os.path.exists(iPath):
-        os.mkdir(iPath)
-    os.chdir(iPath)
-    plt.savefig('{}.{}'.format(name, fmt), fmt='png')
-    os.chdir(pwd)
+import matplotlib as mpl
+print ('Current version on matplotlib library is', mpl.__version__)
